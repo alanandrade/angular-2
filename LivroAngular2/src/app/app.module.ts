224700d49635bc9f044a058ab145ef611e1c535e
Alan Andrade <alan.andradebispo@gmail.com>
2017-05-30 19:05:03 -0300

    atualizado

--- LivroAngular2/src/app/app.module.ts
+++ LivroAngular2/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AlertaService } from './alerta.service';
 import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
 import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 
 @NgModule({ //Para determinar ao Angular 2 que a classe AppModule seja considerada como uma classe de configuração de módulo, temos de colocar o decorador @ngModule
   declarations: [ //todas as classes e componentes que criaremos
     AppComponent,
     ListaPessoaComponent,
     InterpolationBindingComponent,
-    PropertyBindingComponent
+    PropertyBindingComponent,
+    TwoWayBindingComponent
   ],
   imports: [ //módulos internos do Angular 2 e de terceiros
     BrowserModule,
